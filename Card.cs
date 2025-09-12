31af01c4d867047b91097407d79199a3b66478be
Vivanto <1425078256@qq.com>
2025-09-12 15:46:58 +0800

    Created Card and statusCard

---
+++ Card.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class Card : TextureRect
+{
+	public enum Face 
+	{
+		Back,
+		SunnyDay,
+		FriendShip,
+		FriendShipOver,
+		Loss,
+		Shuffle,
+		StandUp,
+		Stay,
+		Steel,
+		SunScreen,
+		SunUmbrella,
+	}
+	private Face face = Face.Back;
+	
+	public Face GetFace() 
+	{
+		return face;
+	}
+	public void SetFace(Face value) 
+	{
+		face = value;
+		string path = "res://Image/" + face.ToString() + ".png";
+        Texture = GD.Load<Texture2D>(path);
+    }
+}
---
+++ StatusCard.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class StatusCard : TextureRect
+{
+    [Signal]
+    public delegate void DieEventHandler();
+
+    private int hearts = 4;
+    public int GetHearts() 
+    {
+        return hearts;
+    }
+    public void DecreasHearts() 
+    {
+        if(hearts <= 0) 
+        {
+            return;
+        }
+        hearts--;
+        if(hearts == 0) 
+        {
+            Texture = GD.Load<Texture2D>("res://Image/StateDie.jpg");
+            EmitSignal(SignalName.Die);
+        }
+        string labelText = "x" + hearts.ToString();
+        GetNode<Label>("label").Text = labelText;
+    }
+
+    public void Reset() 
+    {
+        Texture = GD.Load<Texture2D>("res://Image/StateLive.jpg");
+        hearts = 4;
+        GetNode<Label>("label").Text = "x4";
+    }
+}
